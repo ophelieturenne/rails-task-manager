d89e2983a9fc532123c7ce4a96c1b5cfb97e190e
ophelie turenne <turenneophelie@gmail.com>
2022-10-04 15:45:05 +0400

    Commit completed challenge task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,5 +12,32 @@ class TasksController < ApplicationController
   end
 
   def create
-  end 
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   get "tasks/new", to: "tasks#new", as: :task_new
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
   get "tasks/:id", to: "tasks#show", as: :task
+  delete "tasks/:id", to: "tasks#destroy"
 end
